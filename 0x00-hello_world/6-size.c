b7890630ddd449233e13c4d3d9ea224ca939c945
Crenacia <hlalalamaluleke@gmail.com>
2023-02-16 15:47:03 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,8 @@
 
 /**
  *main - Prints the size of various tpes on
- *the computer it is compiled and run on.
- * Return: Always 0.
+ *the computer it is compiled and run on..
+ *Return: Always 0.
  */
 
 int main(void)
