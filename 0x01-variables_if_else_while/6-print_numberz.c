b5b1ea9a0140b0c1c5fd6e4c756ccc6b5c815865
Crenacia <hlalalamaluleke@gmail.com>
2023-02-17 15:40:50 -0800

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print any number
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+      int c;
+
+      c = 0;
+      while 
+	      (c < 10) {
+		      putchar(c + '0');
+		      c++;
+	      } 
+      putchar('\n');
+      return (0);
+}
+
+       
