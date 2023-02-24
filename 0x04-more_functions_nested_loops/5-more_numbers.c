972da6f71e92b1e49d48e9862c5991e2cb02b00e
Crenacia <hlalalamaluleke@gmail.com>
2023-02-23 17:34:26 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * more_nummbers - print times the numbers since  up to 
- * Return:  times of the numbers since  up to 
+ * more_numbers - print times the numbers since  up to
+ * Return: times of the numbers simce up to  
  */
 
-void  main(void)
+void more_numbers(void)
 {
 	int h, c;
+	
 	for (h = 0; h < 10; h++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
 			if (c > 9)
 			{
-				_putchar((y / 10) + '0');
+				_putchar((c / 10) + '0');
 			}
-			_putchar((y % 10) + '0');
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 	}
