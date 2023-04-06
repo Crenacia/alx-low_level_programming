bc469e66e88afd6fc2848808c89ef8401a261d1d
Crenacia <hlalalamaluleke@gmail.com>
2023-04-05 19:11:35 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,7 +5,7 @@
  * @head: pointer to the first element in the linked list
  *
  * Return: the data inside the elements that was deleted,
- * or 0 if the list is empty
+ * if the linked list is empty return 0
  */
 int pop_listint(listint_t **head)
 {
